d5d5a4655c3d18ad219204bb2bc6657875b1b376
Samanta Mika <samanta.mika@nagarro.com>
2021-12-23 17:53:18 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- TypeScript/test/service.test.ts
+++ TypeScript/test/service.test.ts
@@ -5,13 +5,28 @@ interface GeoService {
   isInNorthAmerica(country: Country): boolean;
 }
 
+enum DeliveryType {
+  Express,
+  Standard,
+}
+
 class ShippingCostService {
   constructor(private readonly geoService: GeoService) {}
-  calculateCost(country: Country): number {
-    const isInCommonMarket = this.geoService.isInCommonMarket(country);
-    if (isInCommonMarket) {
+
+  calculateCost(country: Country, deliveryType?: DeliveryType): number {
+    if (this.geoService.isInCommonMarket(country)) {
       return 5;
     }
+
+    if (this.geoService.isInNorthAmerica(country)) {
+      switch (deliveryType) {
+        case DeliveryType.Express:
+          return 50;
+        case DeliveryType.Standard:
+          return 35;
+      }
+    }
+
     return -1;
   }
 }
@@ -20,7 +35,7 @@ describe("Shipping Cost Service", () => {
   it("should return 5 when country in common market", () => {
     let geoServiceStub: GeoService = {
       isInNorthAmerica: (country: Country) => false,
-      isInCommonMarket: (country: Country) => true
+      isInCommonMarket: (country: Country) => true,
     };
 
     let sut = new ShippingCostService(geoServiceStub);
@@ -28,16 +43,27 @@ describe("Shipping Cost Service", () => {
     expect(sut.calculateCost({ code: "DE" })).toEqual(5);
   });
 
-  it("should return 50 when country in North America", () => {
+  it("should return 50 when country in North America and Delivery Type Express", () => {
     let geoServiceStub: GeoService = {
       isInNorthAmerica: (country: Country) => true,
-      isInCommonMarket: (country: Country) => false
+      isInCommonMarket: (country: Country) => false,
     };
 
     let sut = new ShippingCostService(geoServiceStub);
 
-    expect(sut.calculateCost({ code: "US" })).toEqual(50);
+    expect(sut.calculateCost({ code: "US" }, DeliveryType.Express)).toEqual(50);
   });
 
+  it("should return 50 when country in North America and Delivery Type Standard", () => {
+    let geoServiceStub: GeoService = {
+      isInNorthAmerica: (country: Country) => true,
+      isInCommonMarket: (country: Country) => false,
+    };
 
+    let sut = new ShippingCostService(geoServiceStub);
+
+    expect(sut.calculateCost({ code: "US" }, DeliveryType.Standard)).toEqual(
+      35
+    );
+  });
 });
