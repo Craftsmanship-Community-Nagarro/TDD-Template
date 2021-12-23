1fed0fda8563cb91e836b60b5fbb06973f8bbc3e
GLazlo <garabl@gmail.com>
2021-12-23 17:33:16 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- TypeScript/test/service.test.ts
+++ TypeScript/test/service.test.ts
@@ -1,27 +1,28 @@
-interface Country {
+interface Country {}
+
+interface GeoService {
+  isInCommonMarket(country: Country): boolean;
 }
 
 class ShippingCostService {
-
-    calculateCost(country: Country): number {
-        return -1;
+  constructor(private readonly geoService: GeoService) {}
+  calculateCost(country: Country): number {
+    const isInCommonMarket = this.geoService.isInCommonMarket(country);
+    if (isInCommonMarket) {
+      return 5;
     }
+    return -1;
+  }
 }
 
 describe("Shipping Cost", () => {
+  it("should return 5 when country in common market", () => {
+    let geoServiceStub: GeoService = {
+      isInCommonMarket: (country: Country) => true,
+    };
 
-    it("should return 5 when country in common market", () => {
+    let sut = new ShippingCostService(geoServiceStub);
 
-        let geoServiceStub =
-            {
-                function isInCommonMarket (country : Country) {
-                    return true;
-                }
-            };
-
-        let sut = new ShippingCostService(geoServiceStub);
-
-        expect(sut.calculateCost({code: "DE"})).toEqual(5);
-    });
-
+    expect(sut.calculateCost({ code: "DE" })).toEqual(5);
+  });
 });
