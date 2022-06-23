483a1a03d0ae51406c9f8172cb240bf85ad14aca
MARCO TULIO ÁVILA CERÓN <marco.avila@nagarro.com>
2022-06-23 18:25:52 +0200

    none Added sum

--- Java/src/main/java/org/nagarro/CurrencyConverter.java
+++ Java/src/main/java/org/nagarro/CurrencyConverter.java
@@ -1,12 +1,30 @@
 package org.nagarro;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-public class CurrencyConverter {
+public final class CurrencyConverter {
+
+  private CurrencyConverter() {
+  }
 
   public static Money convert(Money from, Currency to) {
+    if (from.getCurrency().equals(to)) {
+      return from;
+    }
     BigDecimal conversionRate = CurrencyTable.getValue(new ConversionElement(from.getCurrency(), to));
     return new Money(conversionRate.multiply(from.getValue()), to);
   }
 
+  public static Money addCurrencies(Currency output, List<Money> monies) {
+    BigDecimal total =
+            monies
+                    .stream()
+                    .map(money -> convert(money, output))
+                    .map(money -> money.getValue())
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+    return new Money(
+            total,
+            output);
+  }
 }
--- Java/src/test/java/org/nagarro/StockMoneyTest.java
+++ Java/src/test/java/org/nagarro/StockMoneyTest.java
@@ -1,46 +1,59 @@
 package org.nagarro;
 
 import org.assertj.core.data.Percentage;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StockMoneyTest {
 
-    @Test
-    public void multiplyMoney(){
-        Money money = new Money(BigDecimal.valueOf(5), Currency.USD);
-        StockItem stockItem = new StockItem(money, 2);
+  @Test
+  public void multiplyMoney() {
+    Money money = new Money(BigDecimal.valueOf(5), Currency.USD);
+    StockItem stockItem = new StockItem(money, 2);
 
-        assertThat(stockItem.multiplyShares().getValue()).isEqualTo(BigDecimal.valueOf(10));
-        assertThat(stockItem.getMoney().getCurrency()).isEqualTo(Currency.USD);
-    }
+    assertThat(stockItem.multiplyShares().getValue()).isEqualTo(BigDecimal.valueOf(10));
+    assertThat(stockItem.getMoney().getCurrency()).isEqualTo(Currency.USD);
+  }
 
-    @Test
-    public void divideMoney(){
-        Money money = new Money(BigDecimal.valueOf(4002), Currency.USD);
-        StockItem stockItem = new StockItem(money, 4);
+  @Test
+  public void divideMoney() {
+    Money money = new Money(BigDecimal.valueOf(4002), Currency.USD);
+    StockItem stockItem = new StockItem(money, 4);
 
-        assertThat(stockItem.divideShares().getValue().toString()).isEqualTo("1000.50");
-        assertThat(stockItem.getMoney().getCurrency()).isEqualTo(Currency.USD);
-    }
+    assertThat(stockItem.divideShares().getValue().toString()).isEqualTo("1000.50");
+    assertThat(stockItem.getMoney().getCurrency()).isEqualTo(Currency.USD);
+  }
 
-    @Test
-    public void convertCurrencyFromUSDToEUR(){
-        Money money = new Money(BigDecimal.valueOf(4002), Currency.USD);
-        Money moneyInEuro = CurrencyConverter.convert(/*from*/money, /*to*/Currency.EUR);
+  @Test
+  public void convertCurrencyConversionFromUSDToEUR() {
+    Money money = new Money(BigDecimal.valueOf(4002), Currency.USD);
+    Money moneyInEuro = CurrencyConverter.convert(/*from*/money, /*to*/Currency.EUR);
 
-        assertThat(moneyInEuro.getValue()).isCloseTo(BigDecimal.valueOf(3802.46D), Percentage.withPercentage(2));
-    }
+    assertThat(moneyInEuro.getValue()).isCloseTo(BigDecimal.valueOf(3802.46D), Percentage.withPercentage(2));
+  }
 
-    @Test
-    public void convertCorrencyFromKRWToUSD(){
-        Money money = new Money(BigDecimal.valueOf(10000), Currency.KRW);
-        Money moneyInEuro = CurrencyConverter.convert(/*from*/money, /*to*/Currency.USD);
+  @Test
+  public void convertCorrencyConversionFromKRWToUSD() {
+    Money money = new Money(BigDecimal.valueOf(10000), Currency.KRW);
+    Money moneyInEuro = CurrencyConverter.convert(/*from*/money, /*to*/Currency.USD);
 
-        assertThat(moneyInEuro.getValue()).isCloseTo(BigDecimal.valueOf(7.69D), Percentage.withPercentage(2));
-    }
+    assertThat(moneyInEuro.getValue()).isCloseTo(BigDecimal.valueOf(7.69D), Percentage.withPercentage(2));
+  }
+
+  @Test
+  public void testSumDifferentCurrencies() {
+    Money fiveUSD = new Money(BigDecimal.valueOf(5), Currency.USD);
+    Money tenEUR = new Money(BigDecimal.valueOf(10), Currency.EUR);
+
+    Money money = CurrencyConverter.addCurrencies(Currency.USD, Arrays.asList(fiveUSD, tenEUR));
+
+    assertThat(money.getValue().toString()).isEqualTo("15.50");
+    assertThat(money.getCurrency()).isEqualTo(Currency.USD);
+    assertThat(money.getCurrency().getDescription()).isEqualTo("US Dollar");
+    // assertThat(stockItem.getMoney().getCurrency()).isEqualTo(Currency.USD);
+  }
 }
