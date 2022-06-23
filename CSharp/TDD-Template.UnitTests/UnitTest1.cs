13083c4e6b2bff9bc34bb41f6aeddd7da561c7e5
rgerm <roland.germ@admiral.at>
2022-06-23 17:07:38 +0200

    feature: new test for Divide

--- CSharp/TDD-Template.UnitTests/UnitTest1.cs
+++ CSharp/TDD-Template.UnitTests/UnitTest1.cs
@@ -9,4 +9,12 @@ public class UnitTest1
         var dollarInput = new Money() { Amount = 5.0m, Currency = "USD" };
         dollarInput.Multiply(2).Should().Be(new Money() { Amount = 10m, Currency = "USD" });
     }
+
+    [Fact]
+    public void DivideDollars()
+    {
+        var dollarInput = new Money() { Amount = 4002m, Currency = "KRW" };
+
+        dollarInput.Divide(4).Should().Be(new Money() { Amount = 1000.5m, Currency = "KRW" });
+    }
 }
