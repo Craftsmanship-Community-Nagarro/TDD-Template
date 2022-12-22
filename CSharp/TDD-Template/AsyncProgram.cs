04bde5343dc38a1240b3c2c1ae1f866346102d5d
rgerm <roland.germ@admiral.at>
2022-12-22 16:19:15 +0100

    feature:  asnc example

---
+++ CSharp/TDD-Template/AsyncProgram.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+internal class AsyncExamples
+{
+    private static async Task PrepareBreakfastAsync()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        sw.Start();
+
+        var foodPreparation = new List<Task> {
+            PrepareFoodAsync("Coffee"),
+            PrepareFoodAsync("Ham and Eggs"),
+            PrepareFoodAsync("Tea")
+        };
+
+        await Task.WhenAll(foodPreparation);
+
+        sw.Stop();
+        Console.WriteLine("Done in " + sw.ElapsedMilliseconds + " ms");
+    }
+
+    private static async Task PrepareBreakfastAsync1()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        sw.Start();
+
+        await PrepareFoodAsync("Coffee");
+        await PrepareFoodAsync("Ham and Eggs");
+        await PrepareFoodAsync("Tea");
+
+        sw.Stop();
+        Console.WriteLine("Done in " + sw.ElapsedMilliseconds + " ms");
+    }
+
+    private static async Task PrepareFoodAsync(string food)
+    {
+        await Task.Run(() =>
+        {
+            Console.WriteLine("   start preparing " + food + " ...");
+            Thread.Sleep(2000);
+            Console.WriteLine("   end preparing " + food + " ...");
+        });
+        Console.WriteLine("   " + food + " done");
+    }
+}
--- CSharp/TDD-Template/Program.cs
+++ CSharp/TDD-Template/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        AsyncCallDemo();
+    }
+
+    private static void AsyncCallDemo()
+    {
+        Console.WriteLine("Prepare my breakfast (press key to cancel or close)!");
+
+        PrepareBreakfast();
+
+        Console.WriteLine("Breakfast is ready");
+
+        Console.ReadKey();
+    }
+
+    private static void PrepareBreakfast()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        sw.Start();
+
+        PrepareFood("Coffee");
+        PrepareFood("Ham and Eggs");
+        PrepareFood("Tea");
+
+        sw.Stop();
+        Console.WriteLine("Done in " + sw.ElapsedMilliseconds + " ms");
+    }
+
+    private static void PrepareFood(string food)
+    {
+        Console.WriteLine("   start preparing " + food + " ...");
+        Thread.Sleep(2000);
+        Console.WriteLine("   end preparing " + food + " ...");
+    }
+}
