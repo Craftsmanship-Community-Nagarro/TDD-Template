57ed7c799fe2a6e94d11da88da4ef01deffc06d5
GLazlo <garabl@gmail.com>
2023-07-27 18:10:36 +0200

    Yahtzee first tests, game and player functions

---
+++ TypeScript/app/yahtzeeGame.ts
@@ -0,0 +1,38 @@
+export enum Rule {
+    Yahtzee
+}
+
+export class Game {
+    player:Player;
+    constructor() {
+        this.player = new Player();
+    }
+    score = (dieOne:number, dieTwo:number, dieThree:number, dieFour:number, dieFive:number, rule:Rule):number => {
+        this.player.removeRule(rule);
+        let numberArray: number[] = [dieOne, dieTwo, dieThree, dieFour, dieFive];
+
+        if(this.areDiceValuesEqual(numberArray))
+        return 50;
+        else
+        return 0
+    };
+
+    areDiceValuesEqual = (diceValues: number[]): boolean => {
+        // Check if all elements in the array are equal to the first element
+        return diceValues.every((value) => value === diceValues[0]);
+      }
+
+
+}
+
+export class Player {
+    availableRules: Rule[] = [Rule.Yahtzee]
+
+    removeRule = (usedRule: Rule) => {
+        if(this.availableRules.includes(usedRule))
+        this.availableRules.splice(usedRule);
+        else
+        throw new TypeError("Already used rule!");
+    }
+    
+}
--- TypeScript/test/greetings.test.ts
+++ TypeScript/test/greetings.test.ts
@@ -1,6 +1,23 @@
-import {greetings} from "../app/greetings";
+import { Game, Rule } from "../app/yahtzeeGame";
 
-
-it('should greet', () => {
-    expect(greetings()).toBe("Hello World!")
+it('Should be Yahtzee', () => {
+    expect(new Game().score(1,1,1,1,1,Rule.Yahtzee)).toBe(50);
 });
+
+it('Shouldn\'t be Yahtzee', () => {
+    expect(new Game().score(1,2,3,4,5,Rule.Yahtzee)).toBe(0);
+});
+
+it('Should be Yahtzee', () => {
+    expect(new Game().score(2,2,2,2,2,Rule.Yahtzee)).toBe(50);
+});
+
+/*it('Shouldn\'t be Yahtzee if already scored', () => {
+    let game = new Game();
+    game.score(2,2,2,2,2,Rule.Yahtzee);
+    expect(game.score(2,2,2,2,2,Rule.Yahtzee)).toThrow(TypeError);
+});*/
+
+it('throws an error if the version is wrong', () => {
+    expect(() => myParser('0.,4')).toThrowWithCause('INVALID_VERSION')
+  })
